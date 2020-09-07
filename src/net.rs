529ab60c7580f5e52ff24c549822283b3183adaa
Nigecat <nigel@nigelq.com>
2020-09-07 11:29:34 +1000

    Add class struct

--- src/net.rs
+++ src/net.rs
@@ -1,10 +1,12 @@
 /* Anything network related goes here */
 use std::io::Read;
-use select::predicate::Class;
+use std::vec::Vec;
 use reqwest::blocking::Client;
 use select::document::Document;
+use select::predicate::{Class, Name};
 
-pub fn fetch_timetable(username: String, password: String) -> Document {
+/// Fetch a user's timetable
+pub fn fetch_timetable(username: String, password: String) -> Vec<Period> {
     let client = Client::new();
     // The url to the full timetable differs from user to user,
     // so we first fetch the homepage so we can follow a link to it.
@@ -32,5 +34,31 @@ pub fn fetch_timetable(username: String, password: String) -> Document {
 
     body = String::new();
     resp.read_to_string(&mut body).unwrap();
-    return Document::from(body.as_str());
+    let timetable = format_timetable(Document::from(body.as_str()));
+    return timetable;
+}
+
+#[derive(Debug)]
+pub struct Period {
+    /// The time this period starts at, should be a string like '10:35 AM'
+    start_time: String,
+    /// The time this period ends at, should be a string like '10:35 AM'
+    end_time: String,
+    /// The teacher code of this class, e.g 'THTR'
+    teacher: String,
+    /// The location of this class, for online learning this is either 'STUDY' or 'CONF'
+    loc: String,
+    /// The block this class is in, this is usually in the format 'PERIOD 1' but sometimes may be others such as 'ROLL CALL'
+    block: String,
+    /// The name of this class, e.g 'GEOGRAPHY'
+    name: String,
+}
+
+/// Format the raw html of a timetable into something we can process
+fn format_timetable(raw: Document) -> Vec<Period> {
+    let timetable: Vec<Period> = vec![];
+
+    println!("{:#?}", raw.find(Name("table")).last().unwrap());
+
+    return timetable;
 }
